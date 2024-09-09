eb4bb934a07795ecc3008e8ba69bb9ab51bd3ac5
aarkegz <aarkegz@gmail.com>
2024-09-09 01:47:54 +0000

    Deploying to gh-pages from @ arceos-hypervisor/x86_vcpu@b9f412f385a24e3185310b27133112bbff541529 🚀

---
+++ trait.impl/axvcpu/arch_vcpu/trait.AxArchVCpu.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["x86_vcpu",[["impl AxArchVCpu for <a class=\"struct\" href=\"x86_vcpu/struct.VmxArchVCpu.html\" title=\"struct x86_vcpu::VmxArchVCpu\">VmxVcpu</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[151]}
---
+++ trait.impl/axvcpu/percpu/trait.AxArchPerCpu.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["x86_vcpu",[["impl AxArchPerCpu for <a class=\"struct\" href=\"x86_vcpu/struct.VmxArchPerCpuState.html\" title=\"struct x86_vcpu::VmxArchPerCpuState\">VmxPerCpuState</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[174]}
