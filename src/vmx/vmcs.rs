6304f18e52b1fcbf93d1bcc2d9b6a25fd3017f8c
hky1999 <976929993@qq.com>
2025-03-27 10:01:37 +0800

    [fix] delete redundant outputs

--- src/vmx/vmcs.rs
+++ src/vmx/vmcs.rs
@@ -597,7 +597,7 @@ pub fn set_control(
     let allowed0 = cap as u32;
     let allowed1 = (cap >> 32) as u32;
     assert_eq!(allowed0 & allowed1, allowed0);
-    debug!(
+    trace!(
         "set {:?}: {:#x} (+{:#x}, -{:#x})",
         control, old_value, set, clear
     );
