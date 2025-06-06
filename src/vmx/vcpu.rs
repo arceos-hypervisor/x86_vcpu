e25bda3b5c35994e2ec3b79ceb34e54e8621ce2d
Debin <luodeb@outlook.com>
2025-06-06 16:49:24 +0800

    refactor: `AccessWidth` is defined redundantly in both axvcpu and axaddrspace. Remove it from axvcpu.

--- src/vmx/vcpu.rs
+++ src/vmx/vcpu.rs
@@ -9,9 +9,9 @@ use x86::dtables::{self, DescriptorTablePointer};
 use x86::segmentation::SegmentSelector;
 use x86_64::registers::control::{Cr0, Cr0Flags, Cr3, Cr4, Cr4Flags, EferFlags};
 
-use axaddrspace::{GuestPhysAddr, GuestVirtAddr, HostPhysAddr, NestedPageFaultInfo};
+use axaddrspace::{GuestPhysAddr, GuestVirtAddr, HostPhysAddr, NestedPageFaultInfo, device::AccessWidth};
 use axerrno::{AxResult, ax_err, ax_err_type};
-use axvcpu::{AccessWidth, AxArchVCpu, AxVCpuExitReason, AxVCpuHal};
+use axvcpu::{AxArchVCpu, AxVCpuExitReason, AxVCpuHal};
 
 use super::VmxExitInfo;
 use super::as_axerr;
